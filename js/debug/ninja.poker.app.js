30160b763e7d4d7201c714cb816a9ae39f085be8
Chris Love <clove@extremewebworks.com>
2014-01-20 18:43:38 -0500

    adding css & js files

---
+++ js/debug/ninja.poker.app.js
@@ -0,0 +1,49 @@
+
+(function (window, undefined) {
+
+    var ninjaPoker = function (/*customSettings*/) {
+
+        var that = new ninjaPoker.fn.init(/*customSettings*/);
+
+
+        return that;
+    };
+
+    ninjaPoker.fn = ninjaPoker.prototype = {
+
+        constructor: ninjaPoker,
+
+        init: function (/*customSettings*/) {
+            //return a reference to itself so you can chain things later!
+            return this;
+        },
+
+        version: "0.0.1",
+
+        cards: ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"],
+
+        suites: ["&spades;", "&hearts;", "&diams;", "&clubs;"],
+
+        getSuite: function () {
+            return Math.ceil((Math.random() * 4));
+        },
+
+        getCard: function () {
+            return Math.ceil((Math.random() * 13));
+        }
+
+        ////yes you can create chile objects
+        //settings: {
+        //    prop1: "Sample Module"
+        //}
+
+    };
+
+    ninjaPoker.fn.init.prototype = ninjaPoker.fn;
+
+    return (window.ninjaPoker = ninjaPoker);
+
+
+})(window);
+
+
